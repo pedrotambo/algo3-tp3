d6f84b917040a8d958d7153b83b69417f47067de
Nicolas Risso <nicolas.risso@restorando.com>
2017-10-28 17:38:24 -0300

    Básico conteo

--- src/greedy.cpp
+++ src/greedy.cpp
@@ -66,8 +66,47 @@ struct Game{
     int current_player;
 };
 
+// Devuelve un arreglo donde cada índice (i) indica la cantidad de juegos formados de tamaño i
+std::vector<int> lines_lenghts(Game g){
+
+    std::vector<int> result(g.cols, 0);
+
+    for (int j = 0; j < g.rows; j ++) {
+        for (int i = 0; i < g.cols - 1; i++) {
+            int partial_count = 1;
+
+            while (i + 1 < g.cols and g.board[j][i] == g.board[j][i+1] and g.board[i][j] == PLAYER_1) {
+                partial_count++;
+                i++;
+            }
+
+            result[partial_count]++;
+        }
+    }
+
+    for (int j = 0; j < g.cols; j++) {
+        for (int i = 0; i < g.rows - 1; i++) {
+            int partial_count = 1;
+
+            while (i + 1 < g.rows and g.board[i][j] == g.board[i+1][j] and g.board[i][j] == PLAYER_1) {
+                partial_count++;
+                i++;
+            }
+
+            result[partial_count]++;
+        }
+    }
+
+    for (int i = 0; i < result.size(); i++) {
+        std::cerr << "i: " << i << " #: " << result[i] << std::endl;
+    }
+
+    return result;
+}
+
 // Indica que tan buena es el movimiento pasado como parámetro
 int calculate_move_score(Game g, int move) {
+    lines_lenghts(g);
     return 0;
 }
 
@@ -84,17 +123,17 @@ int calculate_move_score(Game g, int move) {
  *    a alguna de los extremos. Entonces de ser 1 buscará colocar la ficha desde los extremos en primer lugar, y si es 0
  *    desde el centro, los valores intermedios... ni idea.
  */
-int greedy_move(Game g, float strategy_weight, float diagonal_weight, float centered_weight) {
+int greedy_move(Game &g, float strategy_weight, float diagonal_weight, float centered_weight) {
     // Las posibles jugadas son las alturas de cada columna en tanto ninguna de ellas haya alcanzado el máximo
 
     int best_movement = -1;
     int best_movement_score = -1;
-    for (int i = 0; i < g.heights.size(); i++) {
+    for (int i = 0; i < g.cols; i++) {
         if (g.heights[i] < g.rows) {
             // Es una jugada posible
 
-            int possible_move = g.heights[i] + 1;
-            int possible_move_score = calculate_move_score(g, possible_move);
+            int possible_move = i;
+            int possible_move_score = calculate_move_score(g, i);
 
             if (best_movement_score < possible_move_score) {
                 best_movement = possible_move;
@@ -106,7 +145,7 @@ int greedy_move(Game g, float strategy_weight, float diagonal_weight, float cent
     return best_movement;
 }
 
-void play(Game g, int movement) {
+void play(Game &g, int movement) {
     int height = g.heights[movement]++;
     g.board[height][movement] = g.current_player;
     if(g.current_player == PLAYER_1)
