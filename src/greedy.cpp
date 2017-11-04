b1c5ca3a0c90948944f8dde4c8cf2f712f27068a
Lucas Rodriguez <rodriguez.lucas.e@gmail.com>
2017-11-04 18:02:18 -0300

    adaptando los parametros en greedy_move()

--- src/greedy.cpp
+++ src/greedy.cpp
@@ -131,18 +131,150 @@ std::vector<int> lenght_lines(Game &g){
     return result;
 }
 
-// Indica que tan buena es el movimiento pasado como parámetro
-int calculate_move_score(Game &g, int movement, std::vector<int>& parameters) {
-    do_move(g, movement);
-    std::vector<int> lines = lenght_lines(g);
+// Devuelve un arreglo donde cada índice (i) indica la calidad de juegos formados de tamaño i
+std::vector<int> quality_lines(Game &g, std::vector< std::vector<int> >& m){
 
-    int score = 0;
-    // Por ahora esto solo se hace con las lineas 
-    for(unsigned int i = 0; i < lines.size() - 1; i++){
-        score += lines[i]*parameters[i];
+    std::vector<int> result(g.c + 1, 0);
+
+    // cuento lineas horizontales
+    for (int i = 0; i < g.rows; i++) {
+        for (int j = 0; j < g.cols - 1; j++) {
+            int partial_count = 1;
+
+            while (j + 1 < g.cols and g.board[i][j] == g.board[i][j+1] and g.board[i][j] == next_player(g.current_player)) {
+                partial_count++;
+                j++;
+            }
+
+            //Esto puede pasar, si tengo una cadena de X y una de Y separadas por un espacio
+            // y pongo la ficha, puedo formar una de X+Y+1 que puede ser mayor que c
+            partial_count = (partial_count > g.c)? g.c : partial_count;
+
+            //result[partial_count]++;
+            // Si es una cadena mayor a 1, la sumo.
+            if(partial_count > 1) result[partial_count]++;
+                
+            // Si es una cadena de uno y es mi ficha, sumo uno (esto lo hago unicamente en horizontal, luego no).
+            if(partial_count == 1 && g.board[i][j] == next_player(g.current_player)) result[1]++;
+        }
     }
 
-    if (lines[g.c] > 0){
+
+    // cuento lineas verticales
+    for (int j = 0; j < g.cols; j++) {
+        for (int i = 0; i < g.rows - 1; i++) {
+            int partial_count = 1;
+
+            while (i + 1 < g.rows and g.board[i][j] == g.board[i+1][j] and g.board[i][j] == next_player(g.current_player)) {
+                partial_count++;
+                i++;
+            }
+
+            partial_count = (partial_count > g.c)? g.c : partial_count;
+            
+            //result[partial_count]++;
+            // Si es una cadena mayor a 1, la sumo.
+            if(partial_count > 1) result[partial_count]++;
+        }
+    }
+
+    // Diagonal /
+    int down = 0;
+    while(down < g.cols - 1){
+        int i = 0;
+        while(i < g.rows - 1 and down + i < g.cols - 1){
+            int partial_count = 1;
+
+            while(i < g.rows - 1 and down + i < g.cols - 1 and g.board[i][i+down] == g.board[i+1][i+down+1] and g.board[i][i+down] == next_player(g.current_player)){
+                partial_count += 1;
+                i += 1;
+            }
+            partial_count = (partial_count > g.c)? g.c : partial_count;
+            if(partial_count > 1) result[partial_count]++;
+            if(partial_count == 1 && g.board[i][i+down] == next_player(g.current_player)) result[1]++;
+            i += 1;
+        }
+        down += 1;
+    }
+    int up = 1;
+    while(up < g.rows - 1){ 
+        int i = 0;
+        while(i < g.cols - 1 and up + i < g.rows - 1){
+            int partial_count = 1;
+
+            while(i < g.cols - 1 and up + i < g.rows - 1 and g.board[i+up][i] == g.board[i+up+1][i+1] and g.board[i+up][i] == next_player(g.current_player)){
+                partial_count += 1;
+                i += 1;
+            }
+            partial_count = (partial_count > g.c)? g.c : partial_count;
+            if(partial_count > 1) result[partial_count]++;
+            if(partial_count == 1 && g.board[i+up][i] == next_player(g.current_player)) result[1]++;
+            i += 1;
+        }
+        up += 1;
+    }
+
+    // Diagonal \ -
+    up = 0;
+    while(up < g.cols - 1){
+        int i = g.rows - 1;
+        int j = 0;
+        while(0 < i and up + j < g.cols - 1){
+            int partial_count = 1;
+
+            while(0 < i and up + j < g.cols - 1 and g.board[i][up+j] == g.board[i-1][up+1+j] and g.board[i][up+j] == next_player(g.current_player)){
+                partial_count += 1;
+                i -= 1;
+                j += 1;
+            }
+            partial_count = (partial_count > g.c)? g.c : partial_count;
+            if(partial_count > 1) result[partial_count]++;
+            if(partial_count == 1 && g.board[i][up+j] == next_player(g.current_player)) result[1]++;
+            i -= 1;
+            j += 1;
+        }
+        up += 1;
+    }
+    down = 0;
+    while(down < g.rows - 1){
+        int i = g.rows - 1 - 1;
+        int j = 0;
+        while(0 < i - down and j < g.cols - 1){
+            int partial_count = 1;
+
+            while(0 < i - down and j < g.cols - 1 and g.board[i-down][j] == g.board[i-down-1][j+1] and g.board[i-down][j] == next_player(g.current_player)){
+                partial_count += 1;
+                i -= 1;
+                j += 1;
+            }
+            partial_count = (partial_count > g.c)? g.c : partial_count;
+            if(partial_count > 1) result[partial_count]++;
+            if(partial_count == 1 && g.board[i-down][j] == next_player(g.current_player)) result[1]++;
+            i -= 1;
+            j += 1;
+        }
+        down += 1;
+    }
+
+    result[0] = 0;
+    result[1] = 0;
+    return result;
+}
+
+// Indica que tan buena es el movimiento pasado como parámetro
+int calculate_move_score(Game &g, int movement, std::vector<int>& p, std::vector<int>& q, std::vector<int>& r, std::vector< std::vector<int> >& m) {
+    do_move(g, movement);
+    std::vector<int> number_of_lines_of_length = lenght_lines(g);
+    std::vector<int> quality_of_lines_of_length = quality_lines(g, m);
+
+    int score = 0;
+
+    // Por ahora esto solo se hace con las lineas 
+    for(unsigned int i = 0; i < number_of_lines_of_length.size() - 1; i++){
+        score += number_of_lines_of_length[i]*p[i];
+    }
+
+    if (number_of_lines_of_length[g.c] > 0){
         // Si esto pasa, si pongo acá, GANO => MAX SCORE
         score = INT_MAX;
     }
@@ -151,8 +283,22 @@ int calculate_move_score(Game &g, int movement, std::vector<int>& parameters) {
     return score;
 }
 
+// weights of lines length                                              --> p
+// weights of lines quality                                             --> q
+// weights of lines length associated with the number of played tiles   --> r
+// matrix weight of (rows X lines length)                               --> m
+void assign_parameters(std::vector<int>& parameters, std::vector<int>& p, std::vector<int>& q, std::vector<int>& r, std::vector< std::vector<int> >& m){
+    p = parameters;
+}
+
 int greedy_move(Game &g, std::vector<int>& parameters) {
     // Las posibles jugadas son las alturas de cada columna en tanto ninguna de ellas haya alcanzado el máximo
+
+    std::vector<int> p;
+    std::vector<int> q;
+    std::vector<int> r;
+    std::vector< std::vector<int> > m;
+    assign_parameters(parameters, p, q, r, m);
 
     int best_movement = -1;
     int best_movement_score = -1;
@@ -160,7 +306,7 @@ int greedy_move(Game &g, std::vector<int>& parameters) {
         if (valid_move(g, i)) {
 
             int possible_move = i;
-            int possible_move_score = calculate_move_score(g, i, parameters);
+            int possible_move_score = calculate_move_score(g, i, p, q, r, m);
 
             if (best_movement_score < possible_move_score) {
                 best_movement = possible_move;
