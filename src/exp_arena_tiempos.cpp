f8cb7803b67f0d6ea5f443e5ea6212917805c6dd
ocslegna <gabrielscotillo@gmail.com>
2017-11-10 12:07:57 -0300

    add d) exp tiempos y peleas.

--- src/exp_arena_tiempos.cpp
+++ src/exp_arena_tiempos.cpp
@@ -9,7 +9,7 @@
 #include "random_blocker.h"
 #include "opt_players.h"
 
-#define DEBUG 1
+// #define DEBUG 1
 
 struct stat info;
 
@@ -93,9 +93,33 @@ char fight_minimax_greedy_vs_random_blocker(Game &g, std::vector<int>& parameter
     return winner(g);
 }
 
-char fight_alfabeta_vs_random(Game &g){
+char fight_minimax_greedy_vs_random(Game &g, std::vector<int>& parameters){
     while(not finished(g)){
-        int movement = (g.current_player == PLAYER_1) ? alfabeta_move(g) : random_move(g);
+        int movement = (g.current_player == PLAYER_1) ? minimax_greedy_move(g, parameters) : random_move(g);
+        do_move(g, movement);
+    }
+    return winner(g);
+}
+
+char fight_random_vs_random_blocker(Game &g){
+    while(not finished(g)){
+        int movement = (g.current_player == PLAYER_1) ? random_move(g) : random_blocker_move(g);
+        do_move(g, movement);
+    }
+    return winner(g);
+}
+
+char fight_random_vs_random(Game &g){
+    while(not finished(g)){
+        int movement = (g.current_player == PLAYER_1) ? random_move(g) : random_move(g);
+        do_move(g, movement);
+    }
+    return winner(g);
+}
+
+char fight_random_blocker_vs_random_blocker(Game &g){
+    while(not finished(g)){
+        int movement = (g.current_player == PLAYER_1) ? random_blocker_move(g) : random_blocker_move(g);
         do_move(g, movement);
     }
     return winner(g);
@@ -111,6 +135,9 @@ void do_fight(Game &g_run, int fight, int &result, std::vector<int> parameters =
         #endif
 
         if (fight == 0) result = fight_minimax_vs_alfabeta(g_run);
+        else if (fight == 8) result = fight_random_vs_random_blocker(g_run);
+        else if (fight == 9) result = fight_random_vs_random(g_run);
+        else if (fight == 10) result = fight_random_blocker_vs_random_blocker(g_run);
     } else {
 
         #ifdef DEBUG
@@ -122,18 +149,19 @@ void do_fight(Game &g_run, int fight, int &result, std::vector<int> parameters =
             std::cout << std::endl;
         #endif
 
-        // Experimentacion B), C)
+        // Experimentacion B), C), D)
         if (fight == 1) result = fight_greedy_vs_random(g_run, parameters);
         else if (fight == 2) result = fight_greedy_vs_random_blocker(g_run, parameters);
         else if (fight == 3) result = fight_greedy_vs_greedy(g_run, parameters, parameters);
         else if (fight == 4) result = fight_greedy_vs_minimax_greedy(g_run, parameters, parameters);
         else if (fight == 5) result = fight_minimax_greedy_vs_minimax_greedy(g_run, parameters, parameters);
         else if (fight == 6) result = fight_minimax_greedy_vs_random_blocker(g_run, parameters);
+        else if (fight == 7) result = fight_minimax_greedy_vs_random(g_run, parameters);
     }
 }
 
 // Medicion de tiempo de pelea.
-void measure_time(Game &g,
+void measure_time(Game g,
     high_resolution_clock::time_point &T_START, high_resolution_clock::time_point &T_END,
     long &T_DURATION,
     int iterations, int &result, int fight, int &games_played, int &games_won, int &games_lost, int &games_tied,
@@ -157,7 +185,7 @@ void measure_time(Game &g,
 // fight y parameters opcionales, son utilizados para experimentaciones que contienen a greedy, etc.
 void run(int _N, int _M, int _c, std::ofstream &of, int fight = 0, const std::vector<int> parameters = {}) {
     assert(of.is_open());
-    assert(fight >= 0 && fight <= 6);
+    assert(fight >= 0 && fight <= 10);
     if (!parameters.empty()) assert((int)parameters.size() == (_c-2) + (_c-1) + (_c-3)*2); // 4-linea == 2 + 3 + 2 == 7 == cantidad de parametros
 
     // Variables de medicion de tiempos.
@@ -169,6 +197,7 @@ void run(int _N, int _M, int _c, std::ofstream &of, int fight = 0, const std::ve
 
     // Inicializacion
     int N = _N, M = _M, c = _c, max_p = N*M, games_played = 0, games_won = 0, games_lost = 0, games_tied = 0, iterations = 1;
+    max_p = N*M;
 
     // Juego base.
     Game g(N, M, c, max_p, PLAYER_1);
@@ -180,7 +209,7 @@ void run(int _N, int _M, int _c, std::ofstream &of, int fight = 0, const std::ve
     if (parameters.empty() and fight == 0) measure_time(g, T_START, T_END, T_DURATION, iterations, result, fight, games_played, games_won, games_lost, games_tied, PLAYER_1);
     else measure_time(g, T_START, T_END, T_DURATION, iterations, result, fight, games_played, games_won, games_lost, games_tied, PLAYER_1, parameters);    
 
-    of << "ida," << N << "," << M << "," << c << "," << max_p << "," << result - '0' << "," << T_DURATION << "\n";
+    of << "ida," << N << "," << M << "," << c << "," << g.max_p << "," << result - '0' << "," << T_DURATION << "\n";
     
     #ifdef DEBUG
         std::cout << "      IDA time: " << T_DURATION << std::endl;
@@ -190,7 +219,7 @@ void run(int _N, int _M, int _c, std::ofstream &of, int fight = 0, const std::ve
     if (parameters.empty() and fight == 0) measure_time(g, T_START, T_END, T_DURATION, iterations, result, fight, games_played, games_won, games_lost, games_tied, PLAYER_2);
     else measure_time(g, T_START, T_END, T_DURATION, iterations, result, fight, games_played, games_won, games_lost, games_tied, PLAYER_2, parameters);
 
-    of << "vuelta," << N << "," << M << "," << c << "," << max_p << "," << result - '0' << "," << T_DURATION << "\n";
+    of << "vuelta," << N << "," << M << "," << c << "," << g.max_p << "," << result - '0' << "," << T_DURATION << "\n";
     
     #ifdef DEBUG
         std::cout << "      VUELTA time: " << T_DURATION << std::endl;
@@ -212,9 +241,12 @@ void exp_a() {
     of_a.close();
     
     of_a.open("csv/exp_a_minimax_vs_alfa.csv", std::ofstream::out | std::ofstream::app);
-    // run(5, 5, 3, of_a);
-    run(4, 4, 3, of_a);
+    run(4, 5, 3, of_a);
     of_a.close();
+
+    of_a.open("csv/exp_a_minimax_vs_alfa.csv", std::ofstream::out | std::ofstream::app);
+    run(5, 5, 3, of_a);
+    of_a.close();    
 }
 
 void exp_b(std::vector<int> &parameters) {
@@ -226,13 +258,17 @@ void exp_b(std::vector<int> &parameters) {
      * 4: greedy vs min_greedy
      * 5: min_greedy vs min_greedy
      * 6: min_greedy vs random_blocker.
+     * 7: minimax_greedy vs random.
+     * 8: random vs random_blocker.
+     * 9: random vs random. 
+     * 10: random_blocker vs random_blocker.      
      */
 
     #ifdef DEBUG
         std::cout << "EXPERIMENTO B: incremento de columnas." << std::endl;
     #endif
 
-    for (int j = 1; j <= 6; ++j) {
+    for (int j = 1; j <= 10; ++j) {
         std::string fight_name;
 
         // Abro el archivo específico.
@@ -251,13 +287,25 @@ void exp_b(std::vector<int> &parameters) {
         } else if (j == 5) {
             fight_name = "csv/exp_b_minimax_greedy_vs_minimax_greedy.csv"; /* PLAYER_1 == minimax_greedy  VS  PLAYER_2 == minimax_greedy */
             of_b.open(fight_name, std::ofstream::out);
-        } else {
+        } else if (j == 6) {
             fight_name = "csv/exp_b_minimax_greedy_vs_random_blocker.csv"; /* PLAYER_1 == minimax_greedy  VS  PLAYER_2 == random_blocker */
+            of_b.open(fight_name, std::ofstream::out);
+        } else if (j == 7) {
+            fight_name = "csv/exp_b_minimax_greedy_vs_random.csv"; /* PLAYER_1 == minimax_greedy  VS  PLAYER_2 == random */
+            of_b.open(fight_name, std::ofstream::out);
+        } else if (j == 8) {
+            fight_name = "csv/exp_b_random_vs_random_blocker.csv"; /* PLAYER_1 == random  VS  PLAYER_2 == random_blocker */
+            of_b.open(fight_name, std::ofstream::out);
+        } else if (j == 9) {
+            fight_name = "csv/exp_b_random_vs_random_csv"; /* PLAYER_1 == random  VS  PLAYER_2 == random */
+            of_b.open(fight_name, std::ofstream::out);
+        } else if (j == 10) {
+            fight_name = "csv/exp_b_random_blocker_vs_random_blocker.csv"; /* PLAYER_1 == random_blocker  VS  PLAYER_2 == random_blocker */
             of_b.open(fight_name, std::ofstream::out);
         }
 
         // Experimentación. 3 <= c <= 7.
-        for (int i = 3; i <= 7; ++i) {
+        for (int i = 3; i <= 10; ++i) {
             // Si es el primero, agrego headers, sino, abro en modo append pues cerrado por iteración anterior.
             if (i == 3) {
                 of_b << "home_away,filas,columnas,c,max_p,ganador,tiempo\n";
@@ -265,7 +313,8 @@ void exp_b(std::vector<int> &parameters) {
                 of_b.open(fight_name, std::ofstream::out | std::ofstream::app);
             }
 
-            run(6, i, 4, of_b, j, parameters);
+            // run(6, i, 4, of_b, j, parameters);
+            run(10, i, 4, of_b, j, parameters);
             of_b.close();
         }
     }
@@ -280,13 +329,17 @@ void exp_c(std::vector<int> &parameters) {
      * 4: greedy vs min_greedy
      * 5: min_greedy vs min_greedy
      * 6: min_greedy vs random_blocker.
+     * 7: minimax_greedy vs random.
+     * 8: random vs random_blocker.
+     * 9: random vs random. 
+     * 10: random_blocker vs random_blocker.      
      */
     
     #ifdef DEBUG
-        std::cout << "EXPERIMENTO B: incremento de columnas." << std::endl;
+        std::cout << "EXPERIMENTO C: incremento de filas." << std::endl;
     #endif
 
-    for (int j = 1; j <= 6; ++j) {
+    for (int j = 1; j <= 10; ++j) {
         std::string fight_name;
 
         // Abro el archivo específico.
@@ -305,13 +358,25 @@ void exp_c(std::vector<int> &parameters) {
         } else if (j == 5) {
             fight_name = "csv/exp_c_minimax_greedy_vs_minimax_greedy.csv"; /* PLAYER_1 == minimax_greedy  VS  PLAYER_2 == minimax_greedy */
             of_c.open(fight_name, std::ofstream::out);
-        } else {
+        } else if (j == 6) {
             fight_name = "csv/exp_c_minimax_greedy_vs_random_blocker.csv"; /* PLAYER_1 == minimax_greedy  VS  PLAYER_2 == random_blocker */
+            of_c.open(fight_name, std::ofstream::out);
+        } else if (j == 7) {
+            fight_name = "csv/exp_c_minimax_greedy_vs_random.csv"; /* PLAYER_1 == minimax_greedy  VS  PLAYER_2 == random */
+            of_c.open(fight_name, std::ofstream::out);
+        } else if (j == 8) {
+            fight_name = "csv/exp_c_random_vs_random_blocker.csv"; /* PLAYER_1 == random  VS  PLAYER_2 == random_blocker */
+            of_c.open(fight_name, std::ofstream::out);
+        } else if (j == 9) {
+            fight_name = "csv/exp_c_random_vs_random_csv"; /* PLAYER_1 == random  VS  PLAYER_2 == random */
+            of_c.open(fight_name, std::ofstream::out);
+        } else if (j == 10) {
+            fight_name = "csv/exp_c_random_blocker_vs_random_blocker.csv"; /* PLAYER_1 == random_blocker  VS  PLAYER_2 == random_blocker */
             of_c.open(fight_name, std::ofstream::out);
         }
 
-        // Experimentación. 3 <= c <= 7.
-        for (int i = 3; i <= 6; ++i) {
+        // Experimentación. 3 <= f <= 6.
+        for (int i = 3; i <= 10; ++i) {
             // Si es el primero, agrego headers, sino, abro en modo append pues cerrado por iteración anterior.
             if (i == 3) {
                 of_c << "home_away,filas,columnas,c,max_p,ganador,tiempo\n";
@@ -319,23 +384,103 @@ void exp_c(std::vector<int> &parameters) {
                 of_c.open(fight_name, std::ofstream::out | std::ofstream::app);
             }
 
-            run(i, 7, 4, of_c, j, parameters);
+            // run(i, 7, 4, of_c, j, parameters);
+            run(i, 10, 4, of_c, j, parameters);
             of_c.close();
         }
     }
 }
 
+void exp_d(std::vector<int> &p1, std::vector<int> &p2, std::vector<int> &p3, std::vector<int> &p4) {
+    std::ofstream of_d;
+
+    /* 1: greedy_vs_random
+     * 2: greedy_vs_random_blocker
+     * 3: greedy_vs_greedy
+     * 4: greedy vs min_greedy
+     * 5: min_greedy vs min_greedy
+     * 6: min_greedy vs random_blocker.
+     * 7: minimax_greedy vs random.
+     * 8: random vs random_blocker.
+     * 9: random vs random. 
+     * 10: random_blocker vs random_blocker.      
+     */
+    
+    #ifdef DEBUG
+        std::cout << "EXPERIMENTO C: incremento de filas." << std::endl;
+    #endif
+
+    for (int j = 1; j <= 10; ++j) {
+        std::string fight_name;
+
+        // Abro el archivo específico.
+        if (j == 1) {
+            fight_name = "csv/exp_d_greedy_vs_random.csv"; /* PLAYER_1 == greedy  VS  PLAYER_2 == random */
+            of_d.open(fight_name, std::ofstream::out);
+        } else if (j == 2) {
+            fight_name = "csv/exp_d_greedy_vs_random_blocker.csv"; /* PLAYER_1 == greedy  VS  PLAYER_2 == random_blocker */
+            of_d.open(fight_name, std::ofstream::out);
+        } else if (j == 3) {
+            fight_name = "csv/exp_d_greedy_vs_greedy.csv"; /* PLAYER_1 == greedy  VS  PLAYER_2 == greedy */
+            of_d.open(fight_name, std::ofstream::out);
+        } else if (j == 4) {
+            fight_name = "csv/exp_d_greedy_vs_minimax_greedy.csv"; /* PLAYER_1 == greedy  VS  PLAYER_2 == minimax_greedy */
+            of_d.open(fight_name, std::ofstream::out);
+        } else if (j == 5) {
+            fight_name = "csv/exp_d_minimax_greedy_vs_minimax_greedy.csv"; /* PLAYER_1 == minimax_greedy  VS  PLAYER_2 == minimax_greedy */
+            of_d.open(fight_name, std::ofstream::out);
+        } else if (j == 6) {
+            fight_name = "csv/exp_d_minimax_greedy_vs_random_blocker.csv"; /* PLAYER_1 == minimax_greedy  VS  PLAYER_2 == random_blocker */
+            of_d.open(fight_name, std::ofstream::out);
+        } else if (j == 7) {
+            fight_name = "csv/exp_d_minimax_greedy_vs_random.csv"; /* PLAYER_1 == minimax_greedy  VS  PLAYER_2 == random */
+            of_d.open(fight_name, std::ofstream::out);
+        } else if (j == 8) {
+            fight_name = "csv/exp_d_random_vs_random_blocker.csv"; /* PLAYER_1 == random  VS  PLAYER_2 == random_blocker */
+            of_d.open(fight_name, std::ofstream::out);
+        } else if (j == 9) {
+            fight_name = "csv/exp_d_random_vs_random_csv"; /* PLAYER_1 == random  VS  PLAYER_2 == random */
+            of_d.open(fight_name, std::ofstream::out);
+        } else if (j == 10) {
+            fight_name = "csv/exp_d_random_blocker_vs_random_blocker.csv"; /* PLAYER_1 == random_blocker  VS  PLAYER_2 == random_blocker */
+            of_d.open(fight_name, std::ofstream::out);
+        }
+
+        // Experimentación. 4 <= c-linea <= 7.
+        for (int i = 4; i <= 7; ++i) {
+            // Si es el primero, agrego headers, sino, abro en modo append pues cerrado por iteración anterior.
+            if (i == 4) {
+                of_d << "home_away,filas,columnas,c,max_p,ganador,tiempo\n";
+            } else {
+                of_d.open(fight_name, std::ofstream::out | std::ofstream::app);
+            }
+
+            // (c, |parameters|), 4 7, 5 11, 6 15, 7 19.
+            if (i == 4) run(10, 10, i, of_d, j, p1);
+            else if (i == 5) run(10, 10, i, of_d, j, p2);
+            else if (i == 6) run(10, 10, i, of_d, j, p3);
+            else if (i == 7) run(10, 10, i, of_d, j, p4);
+
+            of_d.close();
+        }
+    }
+}
+
+
 int main() {
     srand(time(NULL));
-    
+
     // Creo directorio csv/ con todos los permisos en modo 0777 = rwxrwxrwx.
     mkdir("csv", ACCESSPERMS);
 
     /* Tiempos */
-    std::vector<int> parameters = {52, 100, 24, 47, 95, 21, 11};
-    exp_a();  // a) Incremento ambas dimensiones. N=M, c=N-1. // EXP_A TESTEADO OK.
-    exp_b(parameters);  // b) Incremento columnas(M). N=6, M=3, c=4.
-    exp_c(parameters);  // c) Incremento filas(N). N=3, M=7, c=4.
-    
-    // exp_d(); // d) Variar c=[3,4,5] en N=6, M=7.
+    std::vector<int> p1 = {52, 100, 24, 47, 95, 21, 11};
+    std::vector<int> p2 = {52, 100, 24, 47, 95, 21, 11, 67, 31, 57, 25};
+    std::vector<int> p3 = {52, 100, 24, 47, 95, 21, 11, 67, 31, 57, 25, 77, 79, 84, 76};
+    std::vector<int> p4 = {52, 100, 24, 47, 95, 21, 11, 67, 31, 57, 25, 77, 79, 84, 76, 54, 49, 85, 28};
+
+    exp_b(p1);              // b) Incremento columnas(M). N=6, M=3, c=4.
+    exp_c(p1);              // c) Incremento filas(N). N=3, M=7, c=4.
+    exp_d(p1, p2, p3, p4);  // d) Variar c=[4,5,6,7] en N=6, M=7 o 10 10.
+    exp_a();                // a) Exactos.
 }
