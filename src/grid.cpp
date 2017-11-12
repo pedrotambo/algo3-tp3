0c528ea447ce6b851564026c0a900a2f49accf35
Nicolas Risso <nicolas.risso@restorando.com>
2017-11-12 16:17:36 -0300

    De a dos parametros por vez

--- src/grid.cpp
+++ src/grid.cpp
@@ -25,29 +25,29 @@ char fight_greedy_vs_random_blocker(Game &g, std::vector<int>& parameters){
 
 // Devuelve la cantidad de partidas no perdidas del input
 int evaluate_parameters(Game &g, std::vector<int>& input_genome, std::vector< std::vector<int> >& validation_set) {
-//    std::cout << "Por evaluar: " << std::endl;
-//    for(unsigned int j = 0; j < input_genome.size(); j++){
-//        std::cout << input_genome[j] << " ";
-//    }
-//    std::cout << std::endl;
+    std::cout << "Por evaluar: " << std::endl;
+    for(unsigned int j = 0; j < input_genome.size(); j++){
+        std::cout << input_genome[j] << " ";
+    }
+    std::cout << std::endl;
 
     int games_won = 0;
-    for (int i = 0; i < (int)validation_set.size(); i ++) {
-        Game g_home(g.rows, g.cols, g.c, g.max_p, PLAYER_1);
+//    for (int i = 0; i < (int)validation_set.size(); i ++) {
+//        Game g_home(g.rows, g.cols, g.c, g.max_p, PLAYER_1);
+//
+//        int result = fight(g_home, input_genome, validation_set[i]);
+//
+//        if (result == PLAYER_1)
+//            games_won++;
+//
+//        Game g_away(g.rows, g.cols, g.c, g.max_p, PLAYER_2);
+//        result = fight(g_away, input_genome, validation_set[i]);
+//
+//        if (result == PLAYER_1)
+//            games_won++;
+//    }
 
-        int result = fight(g_home, input_genome, validation_set[i]);
-
-        if (result == PLAYER_1)
-            games_won++;
-
-        Game g_away(g.rows, g.cols, g.c, g.max_p, PLAYER_2);
-        result = fight(g_away, input_genome, validation_set[i]);
-
-        if (result == PLAYER_1)
-            games_won++;
-    }
-
-    int iterations_against_blocker = 100;
+    int iterations_against_blocker = 30;
     for (int i = 0; i < iterations_against_blocker; i++) {
         Game g_home(g.rows, g.cols, g.c, g.max_p, PLAYER_1);
         int result = fight_greedy_vs_random_blocker(g_home, input_genome);
@@ -87,6 +87,7 @@ void find_parameters(std::vector<int> &parameters, std::vector< std::vector<int>
                 best_results[j - start_at] = parameters[j];
             }
         }
+        std::cout << i << " de " << std::pow(10, length) << std::endl;
     }
 
     // Fijo los conseguidos y voy por el próximo segmento
@@ -110,8 +111,8 @@ int main() {
     // Hay dos maneras de hacer grid-search, mediante cross-validation o evualuando contra un validation set independiente
     // Optamos por simplicidad por la segunda
 
-    std::vector< std::vector<int> > validation_set(60);
-    for (unsigned int i = 0; i < 60; i++) {
+    std::vector< std::vector<int> > validation_set(30);
+    for (unsigned int i = 0; i < validation_set.size(); i++) {
         std::vector<int> genome(parameters_lenght);
         for(unsigned int j = 0; j < genome.size(); j++){
             genome[j] = random_weight(generator);
@@ -127,15 +128,23 @@ int main() {
         parameters[l] = random_weight(generator);
     }
 
-    std::cout << "Determinando parametros 0, 1 y 2 (p)" << std::endl;
-    find_parameters(parameters, validation_set, g, g.c - 2, 0);
-    std::cout << "Determinando parametros 3, 4, 5 y 6 (q)" << std::endl;
-    find_parameters(parameters, validation_set, g, g.c - 1, 3);
-    std::cout << "Determinando parametro 7 y 8 (s)" << std::endl;
-    find_parameters(parameters, validation_set, g, g.c - 3, 7);
-    std::cout << "Determinando parametro 8 y 9 (t)" << std::endl;
-    find_parameters(parameters, validation_set, g, g.c - 3, 9);
+//    std::cout << "Determinando parametros 0, 1 y 2 (p)" << std::endl;
+//    find_parameters(parameters, validation_set, g, g.c - 2, 0);
+//    std::cout << "Determinando parametros 3, 4, 5 y 6 (q)" << std::endl;
+//    find_parameters(parameters, validation_set, g, g.c - 1, 3);
+//    std::cout << "Determinando parametro 7 y 8 (s)" << std::endl;
+//    find_parameters(parameters, validation_set, g, g.c - 3, 7);
+//    std::cout << "Determinando parametro 8 y 9 (t)" << std::endl;
+//    find_parameters(parameters, validation_set, g, g.c - 3, 9);
 
+
+    // villero as fuck
+    find_parameters(parameters, validation_set, g, 2, 0);
+    find_parameters(parameters, validation_set, g, 2, 2);
+    find_parameters(parameters, validation_set, g, 2, 4);
+    find_parameters(parameters, validation_set, g, 2, 6);
+    find_parameters(parameters, validation_set, g, 2, 8);
+    find_parameters(parameters, validation_set, g, 1, 10);
 
     std::cout << "El mejor arreglo es: ";
     for(unsigned int j = 0; j < parameters.size(); j++){
